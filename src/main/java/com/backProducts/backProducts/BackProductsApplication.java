578adb03308f59390aff76b1e592bbe4e72727ce
hugo bustamante <hugobustamante.ing@gmail.com>
2024-04-11 18:36:11 -0500

    subiendo los archivos del servicio y el archivo de configuracion para el proceso CI/CD

---
+++ src/main/java/com/backProducts/backProducts/BackProductsApplication.java
@@ -0,0 +1,13 @@
+package com.backProducts.backProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackProductsApplication.class, args);
+	}
+
+}
